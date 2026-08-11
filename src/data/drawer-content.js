be82b8af364edde9cf40f18ed1c5c468cd831500
jooeonpark <jooeon427@gmail.com>
2026-08-11 13:53:42 -0400

    - Finished drawer content and layout for mobile

--- src/data/drawer-content.js
+++ src/data/drawer-content.js
@@ -1,4 +1,5 @@
 export const drawerContent = {
+    // ABOUT & ABOUT DETAIL
     aboutMainText: "Brandstyle is privileged to play a variety of roles to further exposure and awareness for the brands we partner with. We’re inspired by our clients to architect meaningful and effective Communications Strategies that involve a multitude of Communications Streams as shown through the cross section of our work below.",
     aboutMissionText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
     aboutServicesText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
@@ -38,5 +39,67 @@ export const drawerContent = {
         "This is not about self-promotion. It’s about structure. When your positioning, messaging, and presence are aligned, visibility becomes a system—not a series of disconnected efforts—and opportunity becomes a natural outcome.",
         "At its core, The Visibility Advantage connects three elements: your authority (what you know), your clarity (how you communicate it), and your distribution (where your ideas show up). When these work together, visibility compounds over time, building familiarity and trust.",
         "Designed for founders, executives, and experts, this framework helps you define your positioning, strengthen your authority signals, and create a sustainable way to share your ideas.",
-    ]
+    ],
+    // SERVICES
+    servicesMainText: "Brandstyle is privileged to play a variety of roles to further exposure and awareness for the brands we partner with. We’re inspired by our clients to architect meaningful and effective Communications Strategies that involve a multitude of Communications Streams as shown through the cross section of our work below.",
+    servicesClientsText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+    servicesSubText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+    servicesSectorCollapsibleContent: [
+        {
+            title: "Sectors",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+        },
+        {
+            title: "Tech",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+        },
+        {
+            title: "Skincare & Wellness",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+        },
+        {
+            title: "Fashion & Sport",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+        },
+        {
+            title: "Real Estate & Interior Design",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+        },
+        {
+            title: "Food & Beverages",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+        },
+        {
+            title: "Firms",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+        },
+    ],
+    // PODCAST
+    podcastMainText: [
+        "The show ALL about networking - both personally and professionally. We're giving networking a reframe!",
+        "Networking isn't just a business function—it's a way to build genuine relationships, unlock new opportunities, and create meaningful connections. In this podcast, we're redefining networking and making it accessible, fun, and, most importantly, real.",
+    ],
+    podcastCards: [
+        {
+            date: "May 20",
+            length: "33 min",
+            description: "31. Dr. Erika Schwartz on Aging, Wellness Trends & Actually Taking Care of Yourself"
+        },
+        {
+            date: "May 13",
+            length: "41 min",
+            description: "30. Another Podcast Episode"
+        }
+    ],
+    podcastSubText: "Brandstyle is privileged to play a variety of roles to further exposure and awareness for the brands we partner with. We’re inspired by our clients to architect meaningful and effective Communications Strategies that involve a multitude of Communications Streams as shown through the cross section of our work below",
+    // TOOLKIT
+    toolkitMainText: "The Visibility Advantage is a modern framework for turning expertise into opportunity through clarity, credibility, and consistency. In today’s landscape, being qualified is not enough—your ideas need to be clearly understood, trusted, and consistently seen by the right people.",
+    toolkitSubtext_1: "This is not about self-promotion. It’s about structure. When your positioning, messaging, and presence are aligned, visibility becomes a system—not a series of disconnected efforts—and opportunity becomes a natural outcome.",
+    toolkitSubtext_2: "At its core, The Visibility Advantage connects three elements: your authority (what you know), your clarity (how you communicate it), and your distribution (where your ideas show up). When these work together, visibility compounds over time, building familiarity and trust.",
+    toolkitSubtext_3: "Designed for founders, executives, and experts, this framework helps you define your positioning, strengthen your authority signals, and create a sustainable way to share your ideas.",
+    toolkitImage_1: "/images/visibility_adv.png",
+    // CONTACT
+    contactMainText: "Contact us now to determine how we can best elevate your communications.",
+    contactEmail_1: "info@brandstyle.com",
+    contactEmail_2: "careers@brandstyle.com",
 }
